f1263b306656b922109c7c89392a39fd8695b4df
Zheng-Xuan <limzhengxuan98@gmail.com>
2022-08-05 10:12:40 +0800

    reset

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import cv2
 import keras 
-import tensorflow as tf
 import numpy as np
 
-# image_keras = keras.utils.load_img('.\\data\\images_dataset\\1.jpg')
-# image_keras = keras.utils.img_to_array(image_keras)
-# print('keras one', image_keras)
+# Read in the classes
+with open("classes.txt", 'r') as f:
+    classes = f.read().split()
 
-# image_cv = cv2.imread('.\\data\\images_dataset\\1.jpg')
-# image_cv = image_cv[...,::-1].astype(np.float32)
-# print('cv one', image_cv)
+# Load model
+model = cv2.dnn.readNet(model= "frozen_models/frozen_graph.pb", framework= "Tensorflow")
 
-net = cv2.dnn.readNetFromTensorflow(".\\model\\saved_model.pb")
-
+# Initiate camera
 cap = cv2.VideoCapture(0)
 
-while True:
-    ret, frame = cap.read()
+while cap.isOpened():
 
-    cv2.imshow("Mahjong Tile Detector", frame)
+    ret, img = cap.read()
+
+    blob = cv2.dnn.blobFromImage(img, size= (320, 240), swapRB= True, scalefactor= 1/255)
+
+    model.setInput(blob)
+
+    output = model.forward()
+
+    prediction = np.argsort(output[0])[::-1][0]
+
+    cv2.putText(img, text= classes[prediction], org= (5, 25), fontFace= cv2.FONT_HERSHEY_SIMPLEX, fontScale= 0.7, color= (0, 255, 0), thickness= 2)
+
+    cv2.imshow("Mahjong tile detection", img)
     cv2.waitKey(1)
